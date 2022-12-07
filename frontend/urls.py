175eeec2b6e6a7eefe6b17694760ada09e301eab
linh2001ht <39437185+linh2001ht@users.noreply.github.com>
2022-12-07 19:13:44 +0000

    init

--- frontend/urls.py
+++ frontend/urls.py
@@ -19,5 +19,5 @@ urlpatterns = [
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', PasswordsChangeView.as_view(template_name='change_password.html'), name='change_password'),
     path('password_success', password_success, name='password_success'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page')
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='profile')
 ]
